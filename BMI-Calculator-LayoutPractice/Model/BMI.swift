f96eabd19e4801ddabf8e51b7a62034fc756427a
dzodzievTemirlan <uccm.timmy@gmail.com>
2020-02-26 15:29:59 +0300

    Completed App

---
+++ BMI-Calculator-LayoutPractice/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI-Calculator
+//
+//  Created by Temirlan Dzodziev on 04.02.2020.
+//  Copyright © 2020 Temirlan Dzodziev. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
